3f3cb637bf6f22f53aa5a997bdff981108cdb0b1
Benjamin Diener <bd@benjamindiener.com>
2025-07-04 10:42:48 +0200

    feat(math): add common math utility functions
    - Added haversineDistance with support for multiple distance units
    - Added distance function for 2D Euclidean distance
    - Added pointInCircle utility
    - Added degreesToRadians and radiansToDegrees
    - Exported DistanceUnit type for use in public API

---
+++ src/math/index.ts
@@ -0,0 +1,7 @@
+// Export all modules from the 'math' directory
+
+export * from './degreesToRadians'
+export * from './distance'
+export * from './haversineDistance'
+export * from './pointInCircle'
+export * from './radiansToDegrees'
