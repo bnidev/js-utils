a1adcc5cdc04396ae52e6f12413957087080e484
Benjamin Diener <bd@benjamindiener.com>
2026-09-12 14:46:46 +0200

    test: disable vitest globals to enforce explicit imports

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true,
+    globals: false,
     environment: 'happy-dom',
     include: ['**/__tests__/**/*.test.ts'],
     coverage: {
