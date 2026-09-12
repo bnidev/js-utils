8b74a6df1e523ff9072eaebaeebadd564364c28a
Benjamin Diener <bd@benjamindiener.com>
2026-09-12 15:52:25 +0200

    feat(math): expose new utilities from math barrel

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,7 +1,11 @@
 // Export all modules from the 'math' directory
 
+export * from './clamp'
 export * from './degreesToRadians'
 export * from './distance'
 export * from './haversineDistance'
+export * from './inRange'
+export * from './lerp'
 export * from './pointInCircle'
 export * from './radiansToDegrees'
+export * from './randomInt'
